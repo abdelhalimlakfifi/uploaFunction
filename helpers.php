8cafa4a4a8115453dc5978db00f52c5407987c61
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2023-06-08 13:17:57 +0100

    initial commit

---
+++ helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+function uploadFile($image, $path)
+{
+    $file_name = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+    $file_name = str_replace(array('\\', '/',':' , '*', '"', "'", ">", "<", "|", '?', '؟', '.', ' ', '-'), '_', $file_name);
+    $file_name = $file_name.date("_d_m_Y_G_i_s");
+    $fileExtenssion = pathinfo($image->getClientOriginalName(), PATHINFO_EXTENSION);
+    $full_name  = $file_name.'.'.$fileExtenssion;
+    $image->storeAs('public/uloaded_files/'.$path, $full_name);
+    return 'storage/'.$path().$full_name;
+}
+
+
+
+
+
